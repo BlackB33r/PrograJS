283abfe2dc6a1ce33b133126506dcdf7da4af605
BlackB33r <BlackB33r@github.com>
2020-03-06 19:07:24 -0700

    Problemas JS

---
+++ problema24.js
@@ -0,0 +1,19 @@
+//problema 24
+//Comprobar si un número mayor o igual que la unidad es primo.
+console.log("Escribe un numero");
+var numero=process.openStdin();
+var ban=true;
+numero.on('data', function(chunk) {
+  for(var i=2;i<chunk;i++){
+      if(chunk%i==0){
+        ban=false;
+      }
+  }
+  if (ban) {
+  console.log("es primo");
+  }else {
+    console.log("no es primo");
+  }
+  ban=true;
+  console.log("Escribe otro numero o CONTROL+C para salir");
+});
